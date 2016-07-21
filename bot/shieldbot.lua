db8b7059a93c123d0b9c8fefdc114fd5342c503f
BooM-amour <lov3_4_3v3r_666@yahoo.com>
2016-07-21 13:17:27 -0700

    Update shieldbot.lua

--- bot/shieldbot.lua
+++ bot/shieldbot.lua
@@ -251,28 +251,21 @@ function create_config( )
     "sh",
     "serverinfo"
     },
-    sudo_users = {80182995},--Sudo users
+    sudo_users = {133362226},--Sudo users
     moderation = {data = 'data/moderation.json'},
-    about_text = [[MeGa shield v2.9
+    about_text = [[BoT v2.9
 An advanced administration bot based on TG-CLI written in Lua
 
-Github:
-https://github.com/hafez16/mega-shield
+
 
 Admins:
-@hafez1116hafez [Developer]
-@amirho3ien911 [Developer]
-@Eblis_alone [Manager]
-@Mmd_hei [sponsor]
+@Goblinor [Developer-Manager-sponser-Founder]
 
 Special thanks to
 SEEDTEAM
-Hextor team
-Magic team
-Avira team
+ViruSTEAM
 
-Our channels
-@shieldTM [persian]
+
 ]],
     help_text_realm = [[
 Realm Commands:
